069ebed9cecc21531810022bd299f5569c4b643a
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-12 13:14:11 -0600

    feature: Added Rocket.Surgery.Nuke

--- .build/Build.cs
+++ .build/Build.cs
@@ -1,72 +1,90 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.IO;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitVersion;
-using Nuke.Common.Tools.MSBuild;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
-using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
+using Rocket.Surgery.Nuke.ContinuousIntegration;
+using Rocket.Surgery.Nuke.GithubActions;
+using Rocket.Surgery.Nuke.Xamarin;
 
 [CheckBuildProjectConfigurations]
-class Build : NukeBuild
+[UnsetVisualStudioEnvironmentVariables]
+class Versions : NukeBuild,
+                    ICanClean,
+                    ICanRestoreXamarin,
+                    ICanBuildXamariniOS,
+                    ICanPackXamariniOS,
+                    ICanTestXamarin,
+                    IHaveConfiguration<Configuration>
 {
+    /// <summary>
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
+    /// </summary>
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Versions>(x => x.Default);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    public AbsolutePath InfoPlist { get; } = RootDirectory / "src" / "Versions.iOS" / "info.plist";
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
-    [GitVersion] readonly GitVersion GitVersion;
+    public string BaseBundleIdentifier { get; } = "com.companyname.Versions";
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TestsDirectory => RootDirectory / "tests";
-    AbsolutePath OutputDirectory => RootDirectory / "output";
+    [Parameter("Configuration to build")]
+    public Configuration Configuration { get; } = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(OutputDirectory);
-        });
+    public TargetPlatform iOSTargetPlatform { get; } = TargetPlatform.iPhoneSimulator;
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-            MSBuild(s => s
-                .SetTargetPath(Solution)
-                .SetTargets("Restore"));
-        });
+    [OptionalGitRepository]
+    public GitRepository? GitRepository { get; }
 
-    Target Compile => _ => _
+    [ComputedGitVersion]
+    public GitVersion GitVersion { get; } = null!;
+    public Target Clean => _ => _.Inherit<ICanClean>(x => x.Clean);
+
+    public Target Restore => _ => _.Inherit<ICanRestoreXamarin>(x => x.Restore).DependsOn(Clean);
+
+    public Target Build => _ => _.Inherit<ICanBuildXamariniOS>(x => x.BuildiPhone)
         .DependsOn(Restore)
+        .DependsOn(Boots)
+        .DependsOn(ModifyInfoPlist);
+
+    public Target ModifyInfoPlist => _ => _.Inherit<ICanBuildXamariniOS>(x => x.ModifyInfoPlist);
+
+    public Target Pack => _ => _.Inherit<ICanPackXamariniOS>(x => x.PackiPhone).DependsOn(Build);
+
+    public Target Test => _ => _
+        .DependsOn(Build)
+        .Executes(() => { });
+
+    public Target Boots => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Restore)
+        .OnlyWhenStatic(() => !IsLocalBuild)
         .Executes(() =>
         {
-            MSBuild(s => s
-                .SetTargetPath(Solution)
-                .SetTargets("Rebuild")
-                .SetConfiguration(Configuration)
-                .SetAssemblyVersion(GitVersion.AssemblySemVer)
-                .SetFileVersion(GitVersion.AssemblySemFileVer)
-                .SetInformationalVersion(GitVersion.InformationalVersion)
-                .SetMaxCpuCount(Environment.ProcessorCount)
-                .SetNodeReuse(IsLocalBuild));
+            DotNetTasks.DotNetToolInstall(cfg => cfg.SetPackageName("Boots").EnableGlobal());
+            ProcessTasks.StartProcess("boots", "--preview Mono");
+            ProcessTasks.StartProcess("boots", "--preview Xamarin.Android");
+            ProcessTasks.StartProcess("boots", "--preview Xamarin.iOS");
         });
 
+    public Target XamariniOS => _ => _
+        .DependsOn(Restore)
+        .DependsOn(Boots)
+        .DependsOn(ModifyInfoPlist)
+        .DependsOn(Build)
+        .DependsOn(Pack);
+
+    public Target Default => _ => _
+        .DependsOn(XamariniOS);
+
+    public Target BuildVersion => _ => _
+       .Inherit<IHaveBuildVersion>(x => x.BuildVersion)
+       .Before(Default)
+       .Before(Clean);
 }
