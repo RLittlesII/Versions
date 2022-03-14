f786a7ebd6d123e9330912126d3f9a756ae8b1e9
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-14 02:20:54 -0500

    build: Installing .net6.0 per job

--- .build/Build.AzurePipelines.CI.cs
+++ .build/Build.AzurePipelines.CI.cs
@@ -6,39 +6,38 @@ using Nuke.Common.CI.AzurePipelines.Configuration;
 using Nuke.Common.Execution;
 
 [AzurePipelines(AzurePipelinesImage.MacOsLatest,
-    FetchDepth = 0,
     TriggerBranchesInclude = new[] { "main" },
     InvokedTargets = new[] { nameof(Default) },
     AutoGenerate = true)]
 partial class Versions
 {
-    public class AzurePipelinesAttribute : Nuke.Common.CI.AzurePipelines.AzurePipelinesAttribute
-    {
-        public AzurePipelinesAttribute(AzurePipelinesImage image, params AzurePipelinesImage[] images)
-            : base(image, images)
-        {
-        }
-
-        public AzurePipelinesAttribute([CanBeNull] string suffix, AzurePipelinesImage image,
-            params AzurePipelinesImage[] images)
-            : base(suffix, image, images)
-        {
-        }
-
-        protected override IEnumerable<AzurePipelinesStep> GetSteps(ExecutableTarget executableTarget,
-            IReadOnlyCollection<ExecutableTarget> relevantTargets, AzurePipelinesImage image)
-        {
-            if (executableTarget.Name == "InstallDotNet")
-            {
-                yield return new AzurePipelineUseDotNetStep() { Version = "6.0.x" };
-            }
-            else
-            {
-                foreach (var azurePipelinesStep in base.GetSteps(executableTarget, relevantTargets, image))
-                {
-                    yield return azurePipelinesStep;
-                }
-            }
-        }
-    }
+    // public class AzurePipelinesAttribute : Nuke.Common.CI.AzurePipelines.AzurePipelinesAttribute
+    // {
+    //     public AzurePipelinesAttribute(AzurePipelinesImage image, params AzurePipelinesImage[] images)
+    //         : base(image, images)
+    //     {
+    //     }
+    //
+    //     public AzurePipelinesAttribute([CanBeNull] string suffix, AzurePipelinesImage image,
+    //         params AzurePipelinesImage[] images)
+    //         : base(suffix, image, images)
+    //     {
+    //     }
+    //
+    //     protected override IEnumerable<AzurePipelinesStep> GetSteps(ExecutableTarget executableTarget,
+    //         IReadOnlyCollection<ExecutableTarget> relevantTargets, AzurePipelinesImage image)
+    //     {
+    //         if (executableTarget.Name == "InstallDotNet")
+    //         {
+    //             yield return new AzurePipelineUseDotNetStep() { Version = "6.0.x" };
+    //         }
+    //         else
+    //         {
+    //             foreach (var azurePipelinesStep in base.GetSteps(executableTarget, relevantTargets, image))
+    //             {
+    //                 yield return azurePipelinesStep;
+    //             }
+    //         }
+    //     }
+    // }
 }
