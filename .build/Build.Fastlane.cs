e4a51d5dd79fe977cdb4cf808062470551c16755
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-19 16:58:42 -0500

    build: removing fastlane install and setting match before modify plist

--- .build/Build.Fastlane.cs
+++ .build/Build.Fastlane.cs
@@ -7,22 +7,12 @@ partial class Versions
 {
     Target Fastlane => _ => _
         .OnlyWhenStatic(AzurePipelinesTasks.IsRunningOnAzurePipelines)
-        .DependsOn(InstallFastlane)
         .DependsOn(FastlaneMatch);
-
-    Target InstallFastlane => _ => _
-        .OnlyWhenStatic(AzurePipelinesTasks.IsRunningOnAzurePipelines)
-        .Executes(() =>
-        {
-            ProcessTasks.StartProcess("brew", "install fastlane", logInvocation: true, logOutput: true);
-        });
 
     Target FastlaneMatch => _ => _
         .OnlyWhenStatic(AzurePipelinesTasks.IsRunningOnAzurePipelines)
-        .DependsOn(InstallFastlane)
         .DependentFor(ArchiveIpa)
-        .After(ModifyInfoPlist)
-        .Before(ArchiveIpa)
+        .Before(ModifyInfoPlist)
         .Executes(() =>
         {
             ProcessTasks.StartProcess("fastlane","match", logInvocation: true, logOutput: true);
