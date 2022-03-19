2153d86c46ff2a0ffd1be69739dd6fd9979d29cd
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-19 14:50:46 -0500

    switching homebrew installation scripts

--- .build/Build.Homebrew.cs
+++ .build/Build.Homebrew.cs
@@ -12,10 +12,10 @@ partial class Versions
     Target InstallHomebrew => _ => _
         .Executes(() =>
         {
-            // FileSystemTasks.EnsureExistingDirectory("homebrew");
-            // ProcessTasks.StartShell(
-            //     "mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew");
+            FileSystemTasks.EnsureExistingDirectory("homebrew");
             ProcessTasks.StartShell(
-                "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)", logInvocation: true, logOutput: true);
+                "mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew", logInvocation: true, logOutput: true);
+            // ProcessTasks.StartShell(
+            //     "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)", logInvocation: true, logOutput: true);
         });
 }
