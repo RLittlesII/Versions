93eba63cf64aff982b42e43ecf50298f5fd338a8
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-23 20:04:00 -0500

    chore: add app center logging

---
+++ src/Versions/MainViewModel.cs
@@ -0,0 +1,12 @@
+using ReactiveMarbles.Mvvm;
+
+namespace Versions
+{
+    /// <summary>
+    /// Represents the View Model for the main view.
+    /// </summary>
+    public class MainViewModel : RxObject
+    {
+        
+    }
+}
