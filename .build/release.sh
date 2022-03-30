5d7b52fd67bce7503512b4ff0dbc836cdb974f46
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-30 09:28:14 -0500

    release: less fail at azure deploy folder

--- .build/release.sh
+++ .build/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod a+x "unpack.sh"
-./unpack.sh Versions.iOS "$BundleIdentifier"
+"$SYSTEM_DEFAULTWORKINGDIRECTORY"/unpack.sh Versions.iOS "$BundleIdentifier"
 #./fastlane.sh
 #./resign.sh Versions.iOS "$Provisioning"
 #./pack.sh
