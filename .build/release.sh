a9d1b5dbed1c975ea863d400ecfd6f591b7b2c48
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-30 03:17:53 -0500

    release: next bash attempt

--- .build/release.sh
+++ .build/release.sh
@@ -1,4 +1,4 @@
-source unpack.sh Versions.iOS $BundleIdentifier
-source fastlane.sh
-source resign.sh Versions.iOS $Provisioning
-source pack.sh
+source /unpack.sh Versions.iOS $BundleIdentifier
+source /fastlane.sh
+source /resign.sh Versions.iOS $Provisioning
+source /pack.sh
