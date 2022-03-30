ce5b16adb0a3dc279c9289425c767022c0d4aff7
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-30 14:58:32 -0500

    release: passing values as parameters
    env no like me!

--- .build/release.sh
+++ .build/release.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+bundle=$(wc -l < $1)
+profile=$(wc -l < $2)
+echo $bundle
+echo $profile
+
 printenv
+
 chmod a+x "unpack.sh"
 chmod a+x "fastlane.sh"
 chmod a+x "resign.sh"
 chmod a+x "pack.sh"
 
-./unpack.sh "Versions.iOS" "$BundleIdentifier"
+./unpack.sh "Versions.iOS" "$bundle"
 ./fastlane.sh
-./resign.sh "Versions.iOS" "$Provisioning"
+./resign.sh "Versions.iOS" "$profile"
 #./pack.sh
