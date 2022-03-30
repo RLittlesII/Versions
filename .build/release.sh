0b303f80df2bcd0154990c98b2c48f9fdcad3a89
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-03-30 09:57:02 -0500

    release: another again with working directory set

--- .build/release.sh
+++ .build/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod a+x "unpack.sh"
-"$System.DefaultWorkingDirectory"/_RLittlesII.Versions/drop/unpack.sh Versions.iOS "$BundleIdentifier"
+./unpack.sh Versions.iOS "$BundleIdentifier"
 #./fastlane.sh
 #./resign.sh Versions.iOS "$Provisioning"
 #./pack.sh
